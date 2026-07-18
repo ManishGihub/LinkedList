797e67c81e25d8c4920d6d3b7b580e3c04d2f8bb
ManishGihub <102681545+ManishGihub@users.noreply.github.com>
2026-07-18 11:45:49 +0530

    Done

--- RemoveFromEnd.java
+++ RemoveFromEnd.java
@@ -1,17 +1,41 @@
-import org.w3c.dom.Node;
+//Remove Nth Node From End of List
 
-public class RemoveFromEnd {
-    public static void main(String[] args) {
-        public ListNode removeNthFromEnd(ListNode head, int n) {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
         if(head == null){
             return null;
         }
 
-        int size = 0; //size
-        Node currNode = ListNode();
+        int size = 0;
+        ListNode currNode = head;
         while(currNode != null){
             currNode = currNode.next;
             size++;
         }
+
+        if(n == size){
+            return head.next;
+        }
+
+        int idxToFind = size - n;
+        ListNode prevNode = head;
+        int i=1;
+        while(i < idxToFind){
+            prevNode = prevNode.next;
+            i++;
+        }
+
+        prevNode.next = prevNode.next.next;
+        return head;
     }
 }
